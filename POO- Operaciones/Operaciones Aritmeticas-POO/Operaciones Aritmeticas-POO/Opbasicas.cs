0c4523b0d6d86c340bdba5dd403654721829f592
Sofia Abigail Sabán Cárcamo <201800090@colsanjose.edu.gt>
2020-08-27 14:54:38 -0600

    POO-Operaciones

---
+++ POO- Operaciones/Operaciones Aritmeticas-POO/Operaciones Aritmeticas-POO/Opbasicas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operaciones_Aritmeticas_POO
+{
+    class Opbasicas
+    {
+        public double Suma(double a, double b)
+        {
+            return (a + b);
+        }
+
+        public double Resta(double a, double b)
+        {
+            return (a - b);
+        }
+        public double Multiplicacion(double a, double b)
+        {
+            return (a * b);
+        }
+        public double Division(double a, double b)
+        {
+            return (a / b);
+        }
+        public double Potencia(double a, double b)
+        {
+            double num;
+            num = Math.Pow(a, b);
+            return (num);
+
+        }
+        public double Radicacion(double a, double b)
+        {
+
+            double raiz = Math.Sqrt(a);
+            return (raiz);
+        }
+    }
+}
